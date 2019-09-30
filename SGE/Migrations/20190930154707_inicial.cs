aab046b418a976be2e816070e6cd0ac30c4b0667
Lucas Valenzuela <lucasbezerra1701@gmail.com>
2019-09-30 17:37:35 -0300

    Add project files.

---
+++ SGE/Migrations/20190930154707_inicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SGE.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoUsuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Perfil = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoUsuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoUsuario");
+        }
+    }
+}
---
+++ SGE/Models/TipoUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGE.Models
+{
+    public class TipoUsuario
+    {
+        public int Id { get; set; }
+        public string Perfil { get; set; }
+    }
+}
