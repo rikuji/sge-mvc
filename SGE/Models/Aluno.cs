80e42353851a84ad3290e63cb1de23cfb7bcd8ed
Lucas Bezerra <lucasbezerra1701@gmail.com>
2019-10-29 07:27:32 -0300

    Aluno-CargoSecretaria-UserSecretaria

---
+++ SGE/Models/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGE.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNasc { get; set; }
+        public string Sexo { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string CEP { get; set; }
+        public string Numero { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
+        public int TipoUsuarioId { get; set; }
+        public Responsavel Responsavel { get; set; }
+        public int ResponsavelId { get; set; }
+    }
+}
---
+++ SGE/Models/CargoSecretaria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGE.Models
+{
+    public class CargoSecretaria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
